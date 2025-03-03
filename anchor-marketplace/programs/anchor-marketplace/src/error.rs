52593aa4b1d219fdc2730ffb64f29b07775f6ee8
Maksym Kupchenko <maxkup19@gmail.com>
2025-03-03 02:48:13 +0100

    fix the rest

--- anchor-marketplace/programs/anchor-marketplace/src/error.rs
+++ anchor-marketplace/programs/anchor-marketplace/src/error.rs
@@ -2,6 +2,10 @@ use anchor_lang::error_code;
 
 #[error_code]
 pub enum MarketplaceError {
-    #[msg("Name too long")]
+    #[msg("Name is Too Long")]
     NameTooLong,
+    #[msg("Collectio is not Valid")]
+    InvalidCollection,
+    #[msg("Collectio is not Verified")]
+    UnverifedCollection,
 }
--- anchor-marketplace/programs/anchor-marketplace/src/lib.rs
+++ anchor-marketplace/programs/anchor-marketplace/src/lib.rs
@@ -13,21 +13,24 @@ pub mod anchor_marketplace {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
-        ctx.accounts.initialize(name, fee, &ctx.bumps)
+        ctx.accounts.init(name, fee, &ctx.bumps)
     }
 
-    pub fn listing(ctx: Context<List>, price: u64) -> Result<()> {
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
         ctx.accounts.create_listing(price, &ctx.bumps)?;
         ctx.accounts.deposit_nft()
     }
 
     pub fn delist(ctx: Context<Delist>) -> Result<()> {
-        ctx.accounts.withdraw_nft()
+        ctx.accounts.withdraw_nft()?;
+        ctx.accounts.close_listing()
     }
 
     pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
-        ctx.accounts.send_sol()?;
-        ctx.accounts.send_nft()?;
-        ctx.accounts.close_mint_vault()
+        ctx.accounts.pay()?;
+        ctx.accounts.transfer_nft()?;
+        ctx.accounts.close_vault_account()?;
+        ctx.accounts.reward_buyer()
+
     }
 }
